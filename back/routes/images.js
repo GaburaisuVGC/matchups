a4a20861f9b0131009362f31529908fdb89b241c
Matthieu BARBE <Matthieu.BARBE@hardis-group.com>
2024-06-28 01:47:17 +0200

    fix: puppeeter

--- back/routes/images.js
+++ back/routes/images.js
@@ -9,7 +9,10 @@ router.get('/', async (req, res) => {
   const url = `https://pokebin.com/${pasteId}`;
 
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
 
