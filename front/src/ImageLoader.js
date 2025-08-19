9ef5abc75c6d6d16295a04af5c8746bafe067ef6
GaburaisuVGC <matthieu.barbe01@gmail.com>
2025-08-19 15:15:22 +0200

    some updates

--- front/src/ImageLoader.js
+++ front/src/ImageLoader.js
@@ -43,7 +43,7 @@ const ImageLoader = ({
   return (
     <>
       {loading && showSkeleton && <Skeleton />}
-      {!error && (
+      {!error && src && (
         <img
           src={src}
           alt={alt}
